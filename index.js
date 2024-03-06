308d339d38aa4ad648f22e83e2217593ab1021bd
KajsaWall <112626806+KajsaWall@users.noreply.github.com>
2024-03-06 21:49:42 +0100

    Årtal, labels, legend, replay fixat

--- index.js
+++ index.js
@@ -21,17 +21,72 @@ let xScale = d3.scaleLinear([0, maxLabourForce * 1.1], [0, wViz]);
 let yScale = d3.scaleLinear([0, maxGDP * 1.1], [hViz, 0]);
 
 let svg = d3.select("body")
-            .append("svg")
-            .attr("width", wSvg)
-            .attr("height", hSvg);
+  .append("svg")
+  .attr("width", wSvg)
+  .attr("height", hSvg);
+
+let yearText = svg.append("text")
+  .attr("x", 600) 
+  .attr("y", 80)        
+  .attr("text-anchor", "end")
+  .attr("font-size", "26px")
+  .attr("fill", "white");
 
 let xAxis = svg.append("g")
   .attr("transform", `translate(${wPadding}, ${hViz + hPadding})`)
-  .call(d3.axisBottom(xScale));
+  .call(d3.axisBottom(xScale))
+  .append("text")
+  .attr("x", 700) 
+  .attr("y", 10) 
+  .attr("text-anchor", "middle")
+  .attr("font-size", "18px")
+  .text("Labour force")
+  .attr("fill", "white");
+
 
 let yAxis = svg.append("g")
   .attr("transform", `translate(${wPadding}, ${hPadding})`)
-  .call(d3.axisLeft(yScale));
+  .call(d3.axisLeft(yScale))
+  .append("text")
+  .attr("transform", "rotate(-90)")
+  .attr("x", 30) 
+  .attr("y", 0)       
+  .attr("text-anchor", "middle")
+  .attr("font-size", "18px")
+  .text("GDP")
+  .attr("fill", "white");
+
+const legendData = [
+  { color: "green", label: "Japan" },
+  { color: "orange", label: "Spain" },
+  { color: "blue", label: "Australia" },
+  { color: "red", label: "Poland" },
+  { color: "purple", label: "Turkey" }
+];
+
+const legend = svg.append("g")
+  .attr("class", "legend")
+  .attr("transform", `translate(800, 100)`);
+
+legend.selectAll("rect")
+  .data(legendData)
+  .enter()
+  .append("rect")
+  .attr("x", 0)
+  .attr("y", (d, i) => i * 50)   
+  .attr("width", 10)
+  .attr("height", 10)
+  .style("fill", d => d.color);
+
+legend.selectAll("text")
+  .data(legendData)
+  .enter()
+  .append("text")
+  .attr("x", 20)
+  .attr("y", (d, i) => i * 50 + 9)
+  .attr("font-size", "12px")
+  .attr("fill", "white")
+  .text(d => d.label);
 
 let currentYear = "2004";
 let yearData = dataset.find(d => d.year === currentYear);
@@ -62,13 +117,15 @@ function renderGraph() {
   svg.selectAll("circle").sort((a, b) => {
     return d3.descending(a.population, b.population);
   });
-
+/*
   function updateDataset() {
     let currentYearInt = parseInt(currentYear);
     if (currentYearInt < 2018) {
       currentYearInt++;
       currentYear = String(currentYearInt);
       yearData = dataset.find(d => d.year === currentYear);
+
+      yearText.text("Year: " + currentYear);
 
       for (let country of yearData.countries) {
         circles.selectAll(`.circle-${country.name}`)
@@ -84,4 +141,47 @@ function renderGraph() {
 
   setInterval(updateDataset, 2000);
   return svg.node();
+}
+*/
+
+//För att få automatisk replay
+function updateDataset() {
+  let currentYearInt = parseInt(currentYear);
+  if (currentYearInt < 2018) {
+    currentYearInt++;
+    currentYear = String(currentYearInt);
+    yearData = dataset.find(d => d.year === currentYear);
+
+    yearText.text("Year: " + currentYear);
+
+    for (let country of yearData.countries) {
+      circles.selectAll(`.circle-${country.name}`)
+        .data(country.cities)
+        .transition()
+        .duration(2000)
+        .attr("cx", d => xScale(d.labourForce))
+        .attr("cy", d => yScale(d.gdp))
+        .attr("r", d => Math.sqrt(d.population) * 0.01)
+    }
+  } else {
+  currentYear = "2004";
+  yearData = dataset.find(d => d.year === currentYear);
+  yearText.text("Year: " + currentYear);
+
+  for (let country of yearData.countries) {
+    circles.selectAll(`.circle-${country.name}`)
+      .data(country.cities)
+      .transition()
+      .duration(2000)
+      .attr("cx", d => xScale(d.labourForce))
+      .attr("cy", d => yScale(d.gdp))
+      .attr("r", d => Math.sqrt(d.population) * 0.01);
+  }
+}
+
+setTimeout(updateDataset, 2000);
+}
+
+updateDataset();
+return svg.node();
 }
